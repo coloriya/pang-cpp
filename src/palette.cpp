81e030458956964ed0639e3313a4c166d4915ee7
Ankur Seth <iankurseth@gmail.com>
2021-05-26 20:53:36 +0530

    now prefixing paths with design name

--- src/palette.cpp
+++ src/palette.cpp
@@ -38,6 +38,7 @@ fs::path pang::Palette::getPngPath (std::string design_name, Resolution *resolut
 	png_path = fs::path(this->app->args_json["paths"]["output"]);
 
 	png_path.append(resolution->getName());
+	png_path.append(design_name);
 	std::string filename = "pang_";
 	filename += std::to_string(this->index);
 	filename += "_" + design_name + "_" + resolution->getSuffix() + ".png";
